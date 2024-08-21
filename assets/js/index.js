76483191fcbd3a252fc15631d69d12c3cdb851e5
Truowng <quockhanhtruong166@gmail.com>
2024-08-22 02:01:22 +0700

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// const headingTitle = document.querySelector(".banner .heading-block .title");
+// const valueTitle = headingTitle.getAttribute("data-title");
+// const valueArray = valueTitle.split("");
+// headingTitle.innerHTML = valueArray
+//   .map((item) => {
+//     return `<span>${item}</span>`;
+//   })
+//   .join("");
+
+// headingChilds = document.querySelectorAll(".banner .heading-block .title span");
+// headingChilds.forEach((child, index) => {
+//   child.style.animation = `fadeIn ease-in-out 1s ${index * 0.5}s`;
+// });
